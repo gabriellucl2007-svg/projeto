a529a02c363a0a7e3c76553120c46ed31c408594
gabriellucl2007-svg <gabriellucl2007@gmail.com>
2026-06-11 23:14:16 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,6 @@
-const supabase = window.supabaseClient;
 
-window.supabaseClient = supabase.createClient(
-  "https://SEU-PROJETO.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzdmxma2RkaGVieHV0dWd0bml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NjYyOTQsImV4cCI6MjA5NjU0MjI5NH0.uOveZk9BJ7WvIgR8E3_Cd65Svq6Nm5r7mfctpxoj3S8"
-  
-);
 // LOGIN
-async function login() {
+aasync function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
@@ -17,16 +11,6 @@ async function login() {
 
   if (error) {
     alert("Erro no login");
-    console.log(error);
-    return;
-  }
-
-  alert("Logado com sucesso!");
-}
-  });
-
-  if (error) {
-    alert("Email ou senha inválidos");
     return;
   }
 
